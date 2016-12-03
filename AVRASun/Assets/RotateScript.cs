bb885d1b5e33c50d059ccb891e3ce791cdb3e1c0
Anton Konovalov <konovalov.anton.39@gmail.com>
2016-12-03 14:35:08 +0300

    Rotation

---
+++ AVRASun/Assets/RotateScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateScript : MonoBehaviour {
+
+    public Transform centrePoint;
+    public float angleSpeed = 1;
+    public float direction = 1;
+
+
+	// Use this for initialization
+	void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.RotateAround(centrePoint.position, transform.up, angleSpeed * Time.deltaTime * direction);
+    }
+}
