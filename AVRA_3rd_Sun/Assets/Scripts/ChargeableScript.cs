f3dfa9b180c30bc820fc2bea0f318707feb31256
Rostislav Smirnov <rostislavsmirnoff@gmail.com>
2016-12-03 16:35:36 +0300

    Added VR Camera

---
+++ AVRA_3rd_Sun/Assets/Scripts/ChargeableScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChargeableScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Charge() {
+		Renderer rend = GetComponent<Renderer>();
+		rend.material.shader = Shader.Find("Specular");
+		rend.material.SetColor("_SpecColor", Color.red);
+	}
+
+	public void Uncharge() {
+		
+	}
+}
---
+++ AVRA_3rd_Sun/Assets/Scripts/RayShooterScript.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RayShooterScript : MonoBehaviour
+{
+
+	private Camera _camera;
+
+	// Use this for initialization
+	void Start ()
+	{
+		_camera = GetComponent<Camera> ();
+
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+			Vector3 point = new Vector3 (_camera.pixelWidth / 2, _camera.pixelHeight / 2, 0);
+			Ray ray = _camera.ScreenPointToRay (point);
+			RaycastHit hit;
+			if (Physics.Raycast (ray, out hit)) {
+				GameObject hitObject = hit.transform.gameObject;
+				ChargeableScript chargeable = hitObject.GetComponent<ChargeableScript>();
+				if (chargeable != null) {
+					chargeable.ReactToHit();
+					Debug.Log ("Target hit");
+				} else {
+					StartCoroutine (SphereIndicator (hit.point));
+			}
+		}
+	}
+
+	private IEnumerator SphereIndicator (Vector3 pos)
+	{
+		GameObject sphere = GameObject.CreatePrimitive (PrimitiveType.Sphere);
+		sphere.transform.position = pos;
+
+		yield return new WaitForSeconds (1);
+
+		Destroy (sphere);
+	}
+}
