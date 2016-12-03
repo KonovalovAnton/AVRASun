e8872dd794ac99a04a168bc4430a8f8bc07c1acb
Rostislav Smirnov <rostislavsmirnoff@gmail.com>
2016-12-03 21:54:13 +0300

    Ray light added

--- AVRA_3rd_Sun/Assets/Scripts/RayShooterScript.cs
+++ AVRA_3rd_Sun/Assets/Scripts/RayShooterScript.cs
@@ -6,7 +6,7 @@ public class RayShooterScript : MonoBehaviour
 	public float chargeSpeed  = 5f;
 
 	private Camera _camera;
-
+	private GameObject flash;
 	// Use this for initialization
 	void Start ()
 	{
@@ -14,21 +14,31 @@ public class RayShooterScript : MonoBehaviour
 
 		Cursor.lockState = CursorLockMode.Locked;
 		Cursor.visible = false;
+
+		flash = transform.GetChild(0).gameObject;//GetComponentInChildren<Transform>().gameObject;
+		flash.SetActive(false);
 	}
 
 	// Update is called once per frame
 	void Update ()
 	{
+			flash.SetActive(false);
 			Vector3 point = new Vector3 (_camera.pixelWidth / 2, _camera.pixelHeight / 2, 0);
 			Ray ray = _camera.ScreenPointToRay (point);
 			RaycastHit hit;
 			if (Physics.Raycast (ray, out hit)) {
+			
 				GameObject hitObject = hit.transform.gameObject;
 				ChargeableScript chargeable = hitObject.GetComponentInChildren<ChargeableScript>();
 				if (chargeable != null) {
-				chargeable.Charge(Time.deltaTime * chargeSpeed);
+					flash.SetActive(true);
+					chargeable.Charge(Time.deltaTime * chargeSpeed);
 					Debug.Log ("Target hit");
-				} 
+				}
+				else {
+				flash.SetActive(false);
+				Debug.Log("Deactivate!");
+			}
 		}
 	}
 }
