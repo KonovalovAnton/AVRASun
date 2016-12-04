51f78e9208daccc98165e69e6576c08472ed6d05
Anton Konovalov <konovalov.anton.39@gmail.com>
2016-12-04 12:42:09 +0300

    UI score

---
+++ AVRA_3rd_Sun/Assets/ScoreShower.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScoreShower : MonoBehaviour {
+
+    Text t;
+	// Use this for initialization
+	void Start () {
+        t = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        t.text = "Score: " + (int)ChargeableScript.score;
+	}
+}
