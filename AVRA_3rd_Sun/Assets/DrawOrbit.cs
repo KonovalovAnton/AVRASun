593f3a4926696826ea347aba6de458ed4385f52e
Anton Konovalov <konovalov.anton.39@gmail.com>
2016-12-30 23:24:04 +0300

    Rotation refactor

---
+++ AVRA_3rd_Sun/Assets/DrawOrbit.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DrawOrbit : MonoBehaviour {
+
+    [SerializeField] GameObject line;
+    private static float scaleFactor = 500;
+
+    Transform planet;
+
+    private static float frequencyFactor = 1000;
+
+    void Start () {
+        planet = transform.FindChild("Planet1");
+        float r = Vector3.Distance(planet.transform.position, transform.position);
+        float x = 0;
+        float y = r;
+
+        for (float i = 0; i < Mathf.PI * 2; i += Mathf.PI / 90 / Mathf.Pow(r, 0.7f) * frequencyFactor)
+        {
+            x = Mathf.Cos(i) * r;
+            y = Mathf.Sin(i) * r;
+            GameObject go = GameObject.Instantiate(line, transform) as GameObject;
+            go.transform.localPosition = new Vector3(x, 0, y);
+            go.transform.localScale = new Vector3(go.transform.localScale.x * Mathf.Pow(r, 0.8f) / scaleFactor,
+                go.transform.localScale.y * Mathf.Pow(r, 0.8f) / scaleFactor,
+                go.transform.localScale.z) * Mathf.Pow(r, 0.8f) / scaleFactor;
+        }
+    }
+}
--- AVRA_3rd_Sun/Assets/Scripts/RotateScript.cs
+++ AVRA_3rd_Sun/Assets/Scripts/RotateScript.cs
@@ -3,37 +3,20 @@ using System.Collections;
 
 public class RotateScript : MonoBehaviour {
 
-    public Transform centrePoint;
-    public float angleSpeed = 1;
-    public float direction = 1;
-    public GameObject line;
-    public float eclipticRotation = 0;
+    Transform centrePoint;
+    
+    [SerializeField] float angleSpeed;
 
-    private float scaleFactor = 500;
-    private float frequencyFactor = 1000;
-
-    // Use this for initialization
     void Start () {
-        float r = Mathf.Sqrt((transform.position.x - centrePoint.position.x) *
-            (transform.position.x - centrePoint.position.x) + (transform.position.z - centrePoint.position.z) *
-            (transform.position.z - centrePoint.position.z));
-        float x = 0;
-        float y = r;
-		for (float i = 0; i < Mathf.PI*2; i+=Mathf.PI/90/ Mathf.Pow(r, 0.7f) * frequencyFactor)
-        {
-			x = Mathf.Cos(i) * r + centrePoint.position.x;
-			y = Mathf.Sin(i) * r + centrePoint.position.y;
-            GameObject go = GameObject.Instantiate(line, new Vector3(x,0,y), Quaternion.identity) as GameObject;
-            go.transform.parent = transform.parent;
-            go.transform.localScale = new Vector3(go.transform.localScale.x * Mathf.Pow(r, 0.8f) / scaleFactor,
-                go.transform.localScale.y * Mathf.Pow(r, 0.8f) / scaleFactor,
-                go.transform.localScale.z) * Mathf.Pow(r, 0.8f) / scaleFactor;
-        }
-        transform.parent.Rotate(new Vector3(0, 0, eclipticRotation));
+        centrePoint = transform.parent;
     }
 	
-	// Update is called once per frame
 	void Update () {
-        transform.RotateAround(centrePoint.position, transform.up, angleSpeed * Time.deltaTime * direction);
+        transform.RotateAround(centrePoint.position, transform.up, angleSpeed * Time.deltaTime);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.parent.position, transform.position);
     }
 }
